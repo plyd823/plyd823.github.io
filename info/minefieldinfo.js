34a66d2a188a9494e4df6974ee31312d3267846b
plyd823 <116851074+plyd823@users.noreply.github.com>
2024-05-05 01:55:07 -0400

    a

--- info/minefieldinfo.js
+++ info/minefieldinfo.js
@@ -1,38 +1,8 @@
-//minefields Page
-var minefieldsInfo = [["East Blackmaws", [,], "/images/minefields/eastblackmawsminefield.jpg"],
-                      ["West Blackmaws", [,], "/images/minefields/westblackmawsminefield.jpg"],
-                      ["North Colossus", [,], "/images/minefields/northcolossusminefield.jpg"],
-                      ["NW Balefire Flatland", [,], "/images/minefields/nwbalefireflatlandminefield.jpg"],
-                      ["North Balefire Flatland", [,], "/images/minefields/northbalefireflatlandminefield.jpg"],
-                      ["West Dry Gustie", [,], "/images/minefields/westdrygustieminefield.jpg"],
-                      ["North Dry Gustie", [,], "/images/minefields/northdrygustieminefield.jpg"],
-                      ["Central Fuel Veins", [,], "/images/minefields/centralfuelveinsminefield.jpg"],
-                      ["NE Fuel Veins ", [,], "/images/minefields/nefuelveinsminefield.jpg"],
-                      ["North Grit Canyons", [,], "/images/minefields/northgritcanyonsminefield.jpg"],
-                      ["South Grit Canyons ", [,], "/images/minefields/southgritcanyonsminefield.jpg"],
-                      ["Central Parch Moon", [,], "/images/minefields/centralparchmoonminefield.jpg"],
-                      ["North Parch Moon", [,], "/images/minefields/northparchmoonminefield.jpg"],
-                      ["East Reek Hills", [,], "/images/minefields/eastreekhillsminefield.jpg"],
-                      ["NW Reek Hills ", [,], "/images/minefields/nwreekhillsminefield.jpg"],
-                      ["East Cadavanaugh", [,], "/images/minefields/eastcadavanaughminefield.jpg"],
-                      ["Central Cadavanaugh", [,], "/images/minefields/centralcadavanaughminefield.jpg"],
-                      ["West Central Chalkies", [,], "/images/minefields/westcentralchalkiesminefield.jpg"],
-                      ["East Central Chalkies ", [,], "/images/minefields/eastcentralchalkiesminefield.jpg"],
-                      ["South Knit Sack", [,], "/images/minefields/southknitsackminefield.jpg"],
-                      ["SE Knit Sack", [,], "/images/minefields/seknitsackminefield.jpg"],
-                      ["South Rot 'N' Rusties", [,], "/images/minefields/southrotnrustiesminefield.jpg"],
-                      ["Central Rot 'N' Rusties", [,], "/images/minefields/centralrotnrustiesminefield.jpg"],
-                      ["Central Wailing Wind", [,], "/images/minefields/centralwailingwindminefield.jpg"],
-                      ["North Wailing Wind", [,], "/images/minefields/northwailingwindminefield.jpg"],
-                      ["South Central Grandrise", [,], "/images/minefields/southcentralgrandriseminefield.jpg"],
-                      ["East Central Grandrise", [,], "/images/minefields/eastcentralgrandriseminefield.jpg"],
-                      ["SE The Heights", [,], "/images/minefields/setheheightsminefield.jpg"],
-                      ["West The Heights", [,], "/images/minefields/westtheheightsminefield.jpg"],
-                      ["East The Dump", [,], "/images/minefields/eastthedumpminefield.jpg"], 
-                                     ];
+import { minefieldsInfo as minefields} from "./info.js";
 
-let minefieldHtmlList = document.getElementById("minefield-list")
-let minefieldListHMTL = `
+export function minefieldsComponent()
+{
+    let minefieldListHMTL = `
     <tr>
         <th style = "text-align: center">
             Name
@@ -43,22 +13,68 @@ let minefieldListHMTL = `
         <th style = "text-align: center" width = "30%">
             Mines (Top is North)
         </th>
-    </tr>`
-for(minefieldItem of minefieldsInfo)
-{
-    minefieldListHMTL += `
-    <tr>
+    </tr>`;
+
+    minefields.forEach(minefield => {
+        minefieldListHMTL += `
+        <tr>
         <td style = "text-align: center; font-size: 120%"> 
-            ${minefieldItem[0]}
+            ${minefield[0]}
         </td>
         <td style = "text-align: center; font-size: 120%">
-            (${minefieldItem[1][0]}, ${minefieldItem[1][1]})
+            (${minefield[1][0]}, ${minefield[1][1]})
         </td>
         <td style = "text-align: center; font-size: 120%">
-            <img src="${minefieldItem[2]}" alt="Image of minefield"></img>
+            <img src="${minefield[2]}" alt="Image of minefield"></img>
+        </td>
+    </tr>
+    `  
+    })
+    return minefieldListHMTL
+}
+
+export function minefieldsByLocationComponent(location) 
+{
+    let minefieldListHMTL = `
+    <tr>
+        <th style = "text-align: center">
+            Name
+        </th>
+        <th style = "text-align: center">
+            Coordinates 
+        </th>
+        <th style = "text-align: center" width = "30%">
+            Mines (Top is North)
+        </th>
+    </tr>`;
+
+    minefields.forEach(minefield => {
+        if (minefield[3].toLowerCase() == location.toLowerCase())
+        {
+            minefieldListHMTL += `
+            <tr>
+        <td style = "text-align: center; font-size: 120%"> 
+            ${minefield[0]}
+        </td>
+        <td style = "text-align: center; font-size: 120%">
+            (${minefield[1][0]}, ${minefield[1][1]})
+        </td>
+        <td style = "text-align: center; font-size: 120%">
+            <img src="${minefield[2]}" alt="Image of minefield"></img>
         </td>
     </tr>
     `
-}
-minefieldHtmlList.innerHTML = minefieldListHMTL
+        }
 
+        
+    })
+    return minefieldListHMTL
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    if(document.getElementById("t_minefields")) document.getElementById("t_minefields").innerHTML = minefieldsComponent();
+    if(document.getElementById("t_minefields_jeet")) document.getElementById("t_minefields_jeet").innerHTML = minefieldsByLocationComponent("jeet");
+    if(document.getElementById("t_minefields_gutgash")) document.getElementById("t_minefields_gutgash").innerHTML = minefieldsByLocationComponent("gutgash");
+    if(document.getElementById("t_minefields_pinkeye")) document.getElementById("t_minefields_pinkeye").innerHTML = minefieldsByLocationComponent("pinkeye");
+    if(document.getElementById("t_minefields_thedumps")) document.getElementById("t_minefields_thedumps").innerHTML = minefieldsByLocationComponent("thedumps");
+})
